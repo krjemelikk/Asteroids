90cbe963808c883e53c116c3e06d9a4afa2c6e04
Mikhail Eltsov <michael.eltsov@gmail.com>
2023-03-03 12:50:01 +0300

    Add Service installer + bind GameStateMachine

---
+++ Assets/Source/Infrastructure/Zenject/ServicesInstaller.cs
@@ -0,0 +1,16 @@
+﻿using Source.Infrastructure.StateMachine;
+using Zenject;
+
+namespace Source.Infrastructure.Zenject
+{
+    public class ServicesInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            GameStateMachine();
+        }
+
+        private void GameStateMachine() =>
+            Container.BindInterfacesTo<GameStateMachine>();
+    }
+}
