b2fe98b6a8621138ce7d2c6690d3a38aaa40699e
Mikhail Eltsov <michael.eltsov@gmail.com>
2023-03-03 16:43:41 +0300

    Add load level state

---
+++ Assets/Source/Infrastructure/StateMachine/States/LoadLevelState.cs
@@ -0,0 +1,31 @@
+﻿using Source.Infrastructure.Services;
+
+namespace Source.Infrastructure.StateMachine.States
+{
+    public class LoadLevelState : IConfigurableState<string>
+    {
+        private readonly IGameStateMachine _stateMachine;
+        private readonly ISceneLoader _sceneLoader;
+
+        public LoadLevelState(IGameStateMachine stateMachine, ISceneLoader sceneLoader)
+        {
+            _stateMachine = stateMachine;
+            _sceneLoader = sceneLoader;
+        }
+        
+        public void Enter(string sceneName)
+        {
+            _sceneLoader.LoadScene(sceneName, onLoaded);
+        }
+
+        public void Exit()
+        {
+            
+        }
+
+        private void onLoaded()
+        {
+            
+        }
+    }
+}
