39ca06e59c488b1b6c95d9a699a72df29b655f98
Mikhail Eltsov <michael.eltsov@gmail.com>
2023-03-10 23:38:53 +0300

    Refactoring

--- Assets/Source/UI/Elements/ActorUI.cs
+++ Assets/Source/UI/Elements/ActorUI.cs
@@ -1,14 +1,15 @@
 using Source.GameLogic;
+using Source.GameLogic.Ship;
 using UnityEngine;
 using Zenject;
 
 public class ActorUI : MonoBehaviour
 {
     [SerializeField] private HpBar _hpBar;
-    private IDamageable _health;
+    private IHealth _health;
 
     [Inject]
-    private void Construct(IDamageable health)
+    private void Construct(IHealth health)
     {
         _health = health;
         _health.HealthChanged += OnHealthChanged;
